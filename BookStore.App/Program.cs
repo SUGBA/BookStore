df81ea639e680831f88d8c442c48fc9bfc3fa4c5
Валиев Антон Дмитриевич <valiev.ad@prg-it.ru>
2023-10-17 09:30:19 +0500

    BS-3 Не получается установить связь многие многим

--- BookStore.App/Program.cs
+++ BookStore.App/Program.cs
@@ -1,3 +1,6 @@
+using BookStore.EF.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookStore.App
 {
     public class Program
@@ -5,6 +8,9 @@ namespace BookStore.App
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
+            builder.Services.AddDbContext<BookStoreContext>(options => options.UseNpgsql(connection));
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
@@ -14,6 +20,9 @@ namespace BookStore.App
             app.UseStaticFiles();
             app.MapRazorPages();
             app.MapDefaultControllerRoute();
+
+
+            app.MapGet("/", (BookStoreContext db) => db.Users.ToList());
 
             app.Run();
         }
